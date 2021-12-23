3092921362d05a30372e1617cd0473477a13b73e
connor-shrader <42718153+connor-shrader@users.noreply.github.com>
2021-12-23 20:34:25 +0000

    Deploying to gh-pages from  @ 2235d07528fe5512e43896dea73d2bbd7e29fa35 🚀

--- blog/functionapproximations/App.js
+++ blog/functionapproximations/App.js
@@ -1,4 +1,4 @@
-import {useState, useMemo} from "react";
+import { useState, useMemo } from "react";
 import {
   Plot,
   PlotPoint,
@@ -9,35 +9,41 @@ import {
   PlotGridLines,
   PlotAxisLabels,
 } from "../../components/Plot";
-import {
-  getNewtonInterpolation, clone2D
-} from "./Interpolation";
+import { getNewtonInterpolation, clone2D } from "./Interpolation";
 
-const initialPoints = [[1, 1], [0, 0], [2, 0], [2, 1]];
+const initialPoints = [
+  [0, 0],
+  [1, 1],
+  [2, 0],
+];
 
 export const App = () => {
   const [points, setPoints] = useState(initialPoints);
 
   const interpolation = useMemo(() => getNewtonInterpolation(points), [points]);
 
-  const createDragHandler = (i) => ((info) => {
-      const pts = clone2D(points);
-      pts[i] = [pts[i][0] + info.plotDx, pts[i][1] + info.plotDy];
-      setPoints(pts);
-    }
-  );
+  const createDragHandler = (i) => (info) => {
+    const pts = clone2D(points);
+    pts[i] = [pts[i][0] + info.plotDx, pts[i][1] + info.plotDy];
+    setPoints(pts);
+  };
 
   return (
-    <Plot
-      plotCenter={{ x: 0, y: 0 }}
-      plotWidth={10}
-    >
+    <Plot plotCenter={{ x: 3, y: 3 }} plotWidth={14.15}>
       <PlotGridLines />
       <PlotAxisLabels />
-      <PlotFunction fun={interpolation}/>
-      {points.map((pt, i) => 
-        <PlotPoint x={pt[0]} y={pt[1]} draggable={true} onDrag={createDragHandler(i)} size={10} />
-      )}
+      <PlotFunction fun={interpolation} className="interpolation" />
+      {points.map((pt, i) => (
+        <PlotPoint
+          x={pt[0]}
+          y={pt[1]}
+          draggable={true}
+          onDrag={createDragHandler(i)}
+          size={10}
+          key={i}
+          className="node"
+        />
+      ))}
     </Plot>
   );
 };

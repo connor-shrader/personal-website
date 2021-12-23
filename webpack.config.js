8f4936dc49c11e5e6727b61b7c0f2a4236afc354
connor-shrader <connorshrader@gmail.com>
2021-12-23 09:28:31 -0500

    Set up npm/babel/webpack/head config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// Setup came from watching:
+// https://www.youtube.com/watch?v=WDpxqopXd9U
+
+const path = require("path");
+
+const globalDevServer = {
+  port: 3010,
+  watchContentBase: true,
+};
+
+const globalModule = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: [/node_modules/, /components/],
+      use: {
+        loader: "babel-loader",
+      },
+    }
+  ],
+};
+
+module.exports = {
+  entry: {
+    functionapproximations: "./blog/functionapproximations/index.js",
+  },
+  output: {
+    filename: "[name]/index.build.js",
+    path: __dirname + "/blog/",
+  },
+  devServer: globalDevServer,
+  module: globalModule,
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+    d3: "d3",
+  },
+};
